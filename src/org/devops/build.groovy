7bcda6c4336b111a14ebae3e9174fec00cd9222a
greatemperorzjq <51010757+greatemperorzjq@users.noreply.github.com>
2020-02-03 15:22:00 +0800

    Update build.groovy

--- src/org/devops/build.groovy
+++ src/org/devops/build.groovy
@@ -6,9 +6,17 @@ def Build(buildType,buildShell){
     def buildTools = ["mvn":"M2","ant":"ANT","gradle":"GRADLE","npm":"NPM"]
     
     println("当前选择的构建类型为 ${buildType}")
-    
-    if()
     buildHome= tool buildTools[buildType]
-    
-    sh "${buildHome}/bin/${buildType} ${buildShell}"
+
+    if("${buildType}" =="npm"){
+        
+        sh """
+           export NODE_HOME=${buildHome}
+           export PATH=\$NODE_HOME/bin:\$PATH
+           ${buildHome}/bin/${buildType} ${buildShell}"""
+    }
+     else {
+        sh "${buildHome}/bin/${buildType} ${buildShell}"
+    }
+   
 }
