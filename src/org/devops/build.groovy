1d471123255748048967261dd94e28f0bdd7f79e
greatemperorzjq <51010757+greatemperorzjq@users.noreply.github.com>
2020-02-03 12:39:58 +0800

    Create build.groovy

---
+++ src/org/devops/build.groovy
@@ -0,0 +1,14 @@
+package org.deveops
+
+
+//构建类型
+def Build(buildType,buildShell){
+    def buildTools = ["mvn":"M2","ant":"ANT","gradle":"GRADLE","npm":"NPM"]
+    
+    println("当前选择的构建类型为 ${buildType}")
+    
+    if()
+    buildHome= tool buildTools[buildType]
+    
+    sh "${buildHome}/bin/${buildType} ${buildShell}"
+}
